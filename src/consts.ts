dc283a6d720e245a47a0c501811f5855ef3bed7a
Mario Yordanov <mariovyordanov@gmail.com>
2025-09-28 21:30:19 +0300

    adjustments

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Web Developer | Blog | Mario Yordanov";
-export const SITE_DESCRIPTION = "Blog for Web Development by Mario Yordanov";
+export const SITE_TITLE = "Web Developer | Mario Yordanov";
+export const SITE_DESCRIPTION = "Web Development by Mario Yordanov";
