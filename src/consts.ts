fee4b7f87104a5ccf0cc4a91d0822c457f601c38
Mario Yordanov <mariovyordanov@gmail.com>
2026-02-16 00:01:10 +0200

    sys theme

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Web Developer | Notes | Mario Yordanov";
-export const SITE_DESCRIPTION = "Notes for Web Development by Mario Yordanov";
+export const SITE_DESCRIPTION = "Notes on Web Development by Mario Yordanov";
